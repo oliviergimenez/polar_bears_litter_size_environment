28503e9b5ff791486b05672042a6ce85a9d802bc
mrn_ncr <marwan.naciripgmail.com>
2021-06-10 10:18:42 +0200

    Tried running an analysis of correlation wiwht different window using climwind. It worked but I don't understand the results.

--- 05_script/correlations_environmental_variables.R
+++ 05_script/correlations_environmental_variables.R
@@ -8,12 +8,14 @@ library(tidyverse)
 library(lubridate)
 library(cowplot)
 library(corrplot)
+library(climwin)
 Sys.setenv(LANG = "en")
 
 
 # A. Linear correlations =======================================================
 
 # ~ 1. Load and process the data -----------------------------------------------
+
 # Cliamtic oscillation
 AO <- read_csv("06_processed_data/AO_data/AO_data_winter_spring_1990-2019.csv")
 NAO <- read_csv("06_processed_data/NAO_data/NAO_data_winter_spring_1990-2019.csv")
@@ -46,7 +48,8 @@ sea_ice <- data.frame(sea_ice,
                 ice_free_days, ice_free_days_previous, ice_free_days_2y_ago)
 
 
-AO_NAO <- AO %>%
+
+AO_NAO_T_SI <- AO %>%
   left_join(x = AO,
             y = NAO,
             by = "year") %>%
@@ -57,56 +60,51 @@ AO_NAO <- AO %>%
                 winter_NAO, prior_winter_NAO, 
                 winter_NAO_2y_ago = two_year_prior_winter_NAO,
                 spring_NAO, prior_spring_NAO, 
-                spring_NAO_2y_ago = two_year_prior_spring_NAO)
+                spring_NAO_2y_ago = two_year_prior_spring_NAO) %>%
+  left_join(x = .,
+            y = T_data,
+            by = "year") %>%
+  left_join(x = .,
+            sea_ice,
+            by = "year") %>%
+  filter(year >= 1992)
+
+AO_NAO_T_SI_not_2y <- AO_NAO_T_SI %>%
+  dplyr::select(-ice_free_days_2y_ago, -day_retreat_2y_ago, -day_advance_2y_ago,
+                -winter_AO_2y_ago, -spring_AO_2y_ago, 
+                -winter_NAO_2y_ago, -spring_NAO_2y_ago)
+
+colnames(AO_NAO_T_SI_not_2y)
 
 
-
-
-
+# ~ 2. Visualize correlations --------------------------------------------------
 
 # Correlations between climate oscillation indexes
-res <- cor(AO_NAO, method = "pearson", use = "complete.obs")
+res <- cor(AO_NAO_T_SI_not_2y[,c(2:9)], method = "pearson", use = "complete.obs")
 res <- round(res, 2)
 corrplot(res, type = "lower")
 
+
 # Correlations among climate oscillation indexes + temperature
-AO_NAO_T <- AO_NAO %>%
-  left_join(x = AO_NAO,
-            y = T_data,
-            by = "year") 
-
-AO_NAO_T <- AO_NAO_T %>%
-  dplyr::select(-year)
-
-res <- cor(AO_NAO_T, method = "pearson", use = "complete.obs")
+res <- cor(AO_NAO_T_SI_not_2y[,c(2:11)], method = "pearson", use = "complete.obs")
 res <- round(res, 2)
 corrplot(res, type = "lower")
 
 library("Hmisc")
-res2 <- rcorr(as.matrix(AO_NAO_T))[[3]]
+res2 <- rcorr(as.matrix(AO_NAO_T_SI_not_2y[,c(2:11)]))[[3]]
 res2 <- res <- round(res2, 3)
 
 
-
-
 # Correlations among sea ice metrics
-res <- cor(sea_ice[,-1], method = "pearson", use = "complete.obs")
+res <- cor(AO_NAO_T_SI_not_2y[,c(12:17)], method = "pearson", use = "complete.obs")
 res <- round(res, 2)
-library(corrplot)
 corrplot(res, type = "lower")
 
 
-# Correlation between sea ice metrics and climate oscillations + temperature
-AO_NAO_T_SI <- AO_NAO_T %>%
-  left_join(x = AO_NAO,
-            y = sea_ice,
-            by = "year") %>%
-  dplyr::select(-year)
+# Correlation between sea ice metrics + climate oscillations + temperature
 
-
-res <- cor(AO_NAO_T_SI, method = "pearson", use = "complete.obs")
+res <- cor(AO_NAO_T_SI_not_2y[,-1], method = "pearson", use = "complete.obs")
 res <- round(res, 2)
-library(corrplot)
 corrplot(res, type = "lower")
 
 
@@ -168,6 +166,7 @@ plot(res.pca, choix = "var", autoLab = "yes")
 dev.off()
 
 
+# 
 # Inertia captured by each axis
 x <- inertia.dudi(res.pca)
 inertia <- data.frame(axis = seq(1, 12, 1),
@@ -186,6 +185,7 @@ ggplot(inertia, aes(x = axis, y = inertia_percent)) +
 
 ggsave("07_results/01_interim_results/correlations/inertia.png",
        width = 6, height = 4)
+
 
 # Percentage of varians captured by each axis
 eig.val <- as.data.frame(res.pca$eig) %>%
@@ -206,3 +206,59 @@ ggsave("07_results/01_interim_results/correlations/percentage_variance.png",
        width = 6, height = 4)
 
 
+# C. Climwin ===================================================================
+
+library(climwin)
+
+# Temperature
+temperature.raw <- read_delim("04_raw_data/temperature/svalbard_temperature_daily_montly_1898-2018.csv", 
+                              ";", escape_double = FALSE, col_types = cols(X6 = col_skip(), 
+                                                                           X7 = col_skip()), trim_ws = TRUE)
+T.processed <- temperature.raw %>%
+  mutate(Month = ifelse(Month < 10, paste0(0, Month), 
+                        Month),
+         Day = ifelse(Day < 10, paste0(0, Day), 
+                      Day),
+         date = paste0(Day, "/", Month, "/", Year),
+         T_daily = as.numeric(str_replace(T_daily, ",", "."))) %>%
+  dplyr::select(month = Month,
+                day = Day,
+                year = Year,
+                date,
+                T_daily) %>%
+  filter(year >= 1990)
+
+# Arctic Oscillation (AO)
+AO_raw <- read_table2("04_raw_data/arctic_oscillation/monthly_actic_oscillation_index.txt", 
+                      col_types = cols(`1950` = col_skip()))
+
+AO_processed <- AO_raw %>%
+  filter(year > 1985,
+         year < 2021) %>%
+  mutate(month = ifelse(month < 10, paste0(0, month),
+                        month),
+         day = 01,
+         date = paste0(day, "/", month, "/", year),
+         positive = ifelse(index > 0, 1, 0)) %>%
+  select(year, index, positive, date, month, day)
+
+
+output <- slidingwin(xvar = list(index = AO_processed$index),
+                     cdate = AO_processed$date, 
+                     bdate = T.processed$date, 
+                     baseline = glm(T_daily ~ 1, data = T.processed),
+                     range = c(24, 0), 
+                     type = "relative", stat = "mean", 
+                     func = c("lin"), 
+                     cmissing = FALSE, 
+                     cinterval = "month") 
+
+
+output[[1]]$BestModel
+output[[1]]$BestModelData
+output[[1]]$Dataset
+output[["combos"]]
+
+?glm
+
+#533747, #5f506b, #6a6b83, #076949f, #86bbbd
